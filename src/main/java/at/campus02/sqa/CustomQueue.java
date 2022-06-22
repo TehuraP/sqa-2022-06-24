e785e103bf6227d68c01a2141de66c5a6d7ac101
Arno Hollosi <ahollosi@xmp.net>
2022-06-22 17:33:11 +0200

    add doc to CustomQueue

--- src/main/java/at/campus02/sqa/CustomQueue.java
+++ src/main/java/at/campus02/sqa/CustomQueue.java
@@ -10,10 +10,17 @@ import java.util.Scanner;
 public class CustomQueue {
     private final ArrayDeque<String> queue = new ArrayDeque<>();
 
+    /**
+     * @return True if queue is empty, false otherwise
+     */
     boolean isEmpty() {
         return queue.isEmpty();
     }
 
+    /**
+     * Return next item from queue
+     * @return next item or null if queue is empty
+     */
     String pop() {
         if (queue.isEmpty()) {
             return null;
@@ -22,6 +29,13 @@ public class CustomQueue {
         }
     }
 
+    /**
+     * Add an item to the queue.
+     * If the item is a blank string or contains a "!" anywhere, then
+     * the item is not added to the queue.
+     * @param item Item to push to queue
+     * @return true if adding was successful, false if item was not added
+     */
     boolean push(String item) {
         if (item.isBlank() || item.contains("!")) {
             return false;
@@ -30,6 +44,13 @@ public class CustomQueue {
         return true;
     }
 
+    /**
+     * Create a CustomQueue and populate it with items from the specified resource file.
+     * @param resource Path to resource file
+     * @return Initialized CustomQueue
+     * @throws URISyntaxException If resource path was not a valid path
+     * @throws FileNotFoundException If resource was not found
+     */
     static CustomQueue fromResource(String resource) throws URISyntaxException, FileNotFoundException {
         URL filePath = CustomQueue.class.getResource(resource);
         assert filePath != null;
