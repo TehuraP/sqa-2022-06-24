305c972dc2215b4e07c9d403dfedf6617752e09a
Arno Hollosi <ahollosi@xmp.net>
2023-04-14 12:42:55 +0200

    Step 3b: add second unit test for Input::handle()

--- src/test/java/at/campus02/sqa/TestInput.java
+++ src/test/java/at/campus02/sqa/TestInput.java
@@ -1,24 +1,32 @@
 package at.campus02.sqa;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-
-import static org.mockito.Mockito.*;
 
 import java.io.PrintWriter;
 import java.util.List;
 import java.util.Scanner;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
 public class TestInput {
+
+    private CustomQueue queue;
+    private PrintWriter output;
+
+    @Before
+    public void setup() {
+        queue = new CustomQueue();
+        output = mock(PrintWriter.class);
+    }
 
     @Test
     public void testNextItemOnEmptyQueue() {
         // given
         Scanner scanner = new Scanner("");
-        PrintWriter output = mock(PrintWriter.class);
         Input input = new Input(scanner, output);
-        CustomQueue queue = new CustomQueue();
         // when
         input.nextItem(queue);
         // then
@@ -29,9 +37,7 @@ public class TestInput {
     public void testHandleNext() {
         // given
         Scanner scanner = new Scanner("n\nn\nq\n");
-        PrintWriter output = mock(PrintWriter.class);
         Input input = new Input(scanner, output);
-        CustomQueue queue = new CustomQueue();
         queue.push("one");
         queue.push("two");
         // when
@@ -40,9 +46,24 @@ public class TestInput {
         ArgumentCaptor<String> capture = ArgumentCaptor.forClass(String.class);
         verify(output, atLeast(2)).println(capture.capture());
         List<String> result = capture.getAllValues();
-        Assert.assertEquals("> one", result.get(0));
-        Assert.assertEquals("> two", result.get(1));
+        assertEquals("> one", result.get(0));
+        assertEquals("> two", result.get(1));
     }
 
-
+    @Test
+    public void testHandleAddingNewItems() {
+        // given
+        queue.push("one");
+        Scanner scanner = new Scanner("two\nthree\nn\nn\nn\nq");
+        Input input = new Input(scanner, output);
+        // when
+        input.handle(queue);
+        // then
+        ArgumentCaptor<String> printed = ArgumentCaptor.forClass(String.class);
+        verify(output, times(3)).println(printed.capture());
+        List<String> result = printed.getAllValues();
+        assertEquals("> one", result.get(0));
+        assertEquals("> two", result.get(1));
+        assertEquals("> three", result.get(2));
+    }
 }
